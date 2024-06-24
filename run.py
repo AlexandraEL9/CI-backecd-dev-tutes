76ba31325b2f2a5fd84a0dbec674654b59fbcf6c
Alexandra Lee <alexandraelee9@gmail.com>
2024-06-24 11:43:01 +0000

    routing 3

--- run.py
+++ run.py
@@ -1,23 +1,16 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+    return "<h1>Hello, </h1><h2>World</h2>"
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True
-    )
+        debug=True)
